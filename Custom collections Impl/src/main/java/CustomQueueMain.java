ee980b3bea925cc57017a71342c8f3482f358162
v-angel.zahariev <v-angel.zahariev@shutterfly.com>
2021-06-21 12:42:58 +0300

    adding CustomQueue

--- Custom collections Impl/src/main/java/CustomQueueMain.java
+++ Custom collections Impl/src/main/java/CustomQueueMain.java
@@ -1,6 +1,28 @@
+import collections.CustomQueue;
+
 public class CustomQueueMain {
 
     public static void main(String[] args){
+
+        CustomQueue queue = new CustomQueue(10);
+
+        queue.add(31);
+        queue.add(49);
+
+        System.out.print(queue.remove());
+        System.out.print(queue.remove());
+
+        queue.add(90);
+        queue.add(81);
+        queue.add(72);
+        queue.add(22); // At this point we got to deal with wrapAround, because rear must be pointing to last position.
+
+        System.out.print("Deleted elements from queue: ");
+        System.out.print(queue.remove()+ " ");
+        System.out.print(queue.remove()+ " ");
+        System.out.print(queue.remove()+ " ");
+        System.out.print(queue.remove()+ " ");
+
 
     }
 
--- Custom collections Impl/src/main/java/collections/CustomQueue.java
+++ Custom collections Impl/src/main/java/collections/CustomQueue.java
@@ -1,5 +1,81 @@
 package collections;
 
-public class CustomQueue {
+public class CustomQueue<T> {
+
+    private T[] elements;
+    private int capacity;
+    private int top;
+    private int front;
+    private int count;
+
+    public CustomQueue(int capacity){
+
+        this.capacity = capacity;
+        this.elements = (T[]) new Object[capacity];
+        this.top=0;
+        this.count = 0;
+        this.front = 0;
+    }
+
+    public CustomQueue(){
+        this(16);
+    }
+
+    public void add(T value){
+        if(isFull()){
+            throw new CustomQueueFullException("Queue is full!");
+        }
+        if(top == capacity){
+            top = 0;
+        }
+        this.elements[top++] = value;
+        this.count++;
+    }
+
+    public T remove(){
+        if(isEmpty()){
+            throw new CustomQueueEmptyException("Queue is empty");
+        }
+        if(front == capacity){
+            front = 0;
+        }
+        count--;
+        return this.elements[this.front++];
+    }
+
+    public int elementsCount(){
+        return this.count;
+    }
+
+    public boolean isEmpty(){
+        return this.count == 0;
+    }
+
+    public boolean isFull(){
+        return this.count == capacity;
+    }
+
+}
+
+class CustomQueueFullException extends RuntimeException{
+
+    public CustomQueueFullException(String message){
+        super(message);
+    }
+
+    public CustomQueueFullException(){
+        super();
+    }
+}
+
+class CustomQueueEmptyException extends RuntimeException{
+
+    public CustomQueueEmptyException(String message){
+        super(message);
+    }
+
+    public CustomQueueEmptyException(){
+        super();
+    }
 
 }
