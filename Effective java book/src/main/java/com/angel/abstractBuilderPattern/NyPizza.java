a4949a8fe7af5f3cca7fc85da676ed39e1ce211a
v-angel.zahariev <v-angel.zahariev@shutterfly.com>
2021-09-29 16:25:17 +0300

    adding abstractBuilder pattern

---
+++ Effective java book/src/main/java/com/angel/abstractBuilderPattern/NyPizza.java
@@ -0,0 +1,33 @@
+package com.angel.abstractBuilderPattern;
+
+import java.util.Objects;
+
+public class NyPizza extends Pizza{
+
+    public enum Size { SMALL, MEDIUM, LARGE }
+    private final Size size;
+
+    private static class Builder extends Pizza.Builder<Builder>{
+
+        private final Size size;
+
+        public Builder (Size size){
+            this.size = Objects.requireNonNull(size);
+        }
+
+        @Override
+        protected Builder self() {//implementation of abstract methods
+            return this;
+        }
+
+        @Override
+        Pizza build() {
+            return new NyPizza(this);//here we give an instance of builder to private constructor and this instance is passed to Pizza super() parent constructor
+        }//"this" is equal to builder instance
+    }
+
+    private NyPizza(Builder builder) {
+        super(builder);
+        this.size = builder.size;
+    }
+}
---
+++ Effective java book/src/main/java/com/angel/abstractBuilderPattern/Pizza.java
@@ -0,0 +1,26 @@
+package com.angel.abstractBuilderPattern;
+
+import java.util.EnumSet;
+import java.util.Objects;
+import java.util.Set;
+
+public abstract class Pizza {
+    public enum Topping {HAM, MUSHROOM, ONION, PAPER, SAUSIGE};
+    final Set<Topping> toppings;
+
+    abstract static class Builder<T extends Builder>{//generic type with recursive type parameter
+        EnumSet<Topping> toppings = EnumSet.noneOf(Topping.class);//empty collection
+
+        public T addTopping(Topping topping) {
+            toppings.add(Objects.requireNonNull(topping));
+            return self();//this is in order to chain and add different toppings that is why it self() returns "this" which refer to child class "this returns same object of child class"
+        }//builders can aggregate the parameters passed into multiple calls to a method into a single field, as demonstrated in the addTopping method earlier.
+
+        protected abstract T self();//this will wrap this in child classes
+        abstract Pizza build();//that way every child class will implement it and will return its own pizza variant
+    }
+    Pizza(Builder<?> builder){
+        this.toppings = builder.toppings.clone();//defensive copy
+    }//here we add toppings to toppings field from builder object passed from child class
+
+}
